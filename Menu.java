82f944033235ba909cb86a6a751c91dc5cb0924e
Martin Lidgren <martin.lidgren@live.se>
2023-09-11 13:35:09 +0200

    Changed the menu

--- Menu.java
+++ Menu.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Menu {
 
-    public static void main(String[] args) {
-        int userChoice = Task7.menu();
+    static Scanner scanner = new Scanner(System.in);
+    static int userInput;
+    static int[] studentScores;
 
+    public static void main(String[] args) {
+       Task1.storeStudentScores();
+       menu();
+    }
+
+    public static void menu(){
+        
+        System.out.println("\n Welcome to the menu. Choose one of the options below: \n");
+        
+        System.out.println("1. Register new scores for students.");
+        System.out.println("2. Print all registered grades.");
+        System.out.println("3. Calculate the mean of the entered scores.");
+        System.out.println("4. Find the two highest and two lowest scores.");
+        System.out.println("5. Find the highest score and its position.");
+        System.out.println("6. Collect hashtags from a post.");
+        System.out.println("7. To exit. \n");
+
+        System.out.print("Type your option: ");
+
+        userInput = scanner.nextInt();
+        executeTask(userInput);
+    }
+
+    public static void executeTask(int userChoice){
         switch (userChoice){
             case 1: 
+            studentScores = Task1.storeStudentScores();
+            menu();
             break;
 
             case 2: 
@@ -27,9 +56,9 @@ public class Menu {
 
             default:
             System.out.println("Thank you for using our grading system. Have a nice day!");
+            break;
         
         }
-    
     }
 
 }
--- Task1.java
+++ Task1.java
@@ -1,5 +1,11 @@
 public class Task1 {
+    static int[] studentScores = new int[7];
     
-    
-    
+    public static int[] storeStudentScores(){
+        
+        
+        
+        return studentScores;
+    }
+
 }
