a4bf15f5f14cf39de540375b106969e8e88ef738
Martin Lidgren <martin.lidgren@live.se>
2023-09-11 12:52:44 +0200

    Created classes

--- Menu.java
+++ Menu.java
@@ -1,5 +1,36 @@
 public class Menu {
+
     public static void main(String[] args) {
+        int userChoice = Task7.menu();
+
+        switch (userChoice){
+            case 1: 
+            break;
+
+            case 2: 
+            break;
+
+            case 3:
+            break;
+
+            case 4:
+            break;
+
+            case 5:
+            break;
+
+            case 6:
+            break;
+
+            case 7:
+            break;
+
+            default:
+            System.out.println("Thank you for using our grading system. Have a nice day!");
         
+        }
+    
     }
+
 }
+
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Assignment1-GU
+
+
+Love
+- Task 1
+- Task 2
+
+Emma
+- Task 3
+- Task 4
+
+Martin
+- Task 7
+- Task 5
+
+Collab 
+- Task 6
