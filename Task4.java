73b5ff740f0a7ab9a58fc51eb249571722d43dd0
Emmaolmas <emma.olmas@gmail.com>
2023-09-12 14:58:09 +0200

    Task3

--- Task4.java
+++ Task4.java
@@ -1,9 +1,23 @@
 public class Task4 {
-    
+
+    public static void highestAndLowestScore(int[] studentScores) {
+
+        int highest = studentScores[0];
+        int secondHighest = studentScores[0];
+        int lowest = studentScores[0];
+        int secondLowest = studentScores[0];
+
+        for (int i = 0; i < studentScores.length; i++) {
+
+            if (highest < studentScores[i]) {
+                highest = studentScores[i];
+            }
+            if (lowest > studentScores[i]) {
+                lowest = studentScores[i];
+            }
+        }
+        System.out.println("The two lowest scores provided are " + lowest + ", and " + secondLowest);
+        System.out.println("The two highest scores provided are " + highest + " ,and " + secondHighest);
+
+    }
 }
-
-
-
-
-//“The two lowest scores provided are <lowest>, and <second_lowest>”
-//“The two highest scores provided are <highest>, and <second_highest>” 
