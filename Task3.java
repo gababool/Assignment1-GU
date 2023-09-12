295ca2ce68c40c957c7d67549741f2884f425bca
Emmaolmas <emma.olmas@gmail.com>
2023-09-12 13:27:42 +0200

    Locale

--- Task3.java
+++ Task3.java
@@ -1,14 +1,15 @@
+import java.util.Locale;
 public class Task3 {
 
     public static void meanOfScores(int[] studentScores) {
         int totalScore = 0;
 
-        int scores = 0;
         for (int i = 0; i < studentScores.length; i++) {
-            scores += studentScores[i];
-
-            totalScore = totalScore + scores;
-
+            
+            totalScore += studentScores[i];
         }
+         double mean = (double)totalScore / studentScores.length;
+
+         System.out.printf(Locale.ENGLISH, "The mean of the number is %.2f ", mean, ".");
     }
 }
--- Task4.java
+++ Task4.java
@@ -1,3 +1,9 @@
 public class Task4 {
     
 }
+
+
+
+
+//“The two lowest scores provided are <lowest>, and <second_lowest>”
+//“The two highest scores provided are <highest>, and <second_highest>” 
