8ec01280a7d43cc03c3c746017fe7f84dc628483
Martin Lidgren <martin.lidgren@live.se>
2023-09-12 13:09:12 +0200

    Added method to Task1

--- Task1.java
+++ Task1.java
@@ -14,15 +14,10 @@ public class Task1 {
     public static int[] storeStudentScores() {
 
         for (int i = 0; i <= studentScores.length - 1; i++) {
-            if (i <= ordinalIndicators.length - 2) {
-                System.out.print("Enter the score of the " + (i + 1) + ordinalIndicators[i] + " student: ");
-                studentScores[i] = input.nextInt();
-                input.nextLine();
-            } else {
-                System.out.print("Enter the score of the " + (i + 1) + ordinalIndicators[3] + " student: ");
-                studentScores[i] = input.nextInt();
-                input.nextLine();
-            }
+            System.out.print("Enter the score of the " + addOrdinalIndicator(i+1) + " student: ");
+            studentScores[i] = input.nextInt();
+            input.nextLine();
+           
             if (studentScores[i] < 0 || studentScores[i] > 100) {
                 System.out.println("Error - Input out of bounds. Enter a score between 0 and 100.");
                 i--;
@@ -44,4 +39,18 @@ public class Task1 {
         System.out.println("Your entered scores are: \n" + scores);
     }
 
+    public static String addOrdinalIndicator(int number){
+        int lastDigit = number % 10;
+        switch (lastDigit) {
+        case 1:
+            return number + "st";
+        case 2:
+            return number + "nd";
+        case 3:
+            return number + "rd";
+        default:
+            return number + "th";
+    }
+
+}
 }
