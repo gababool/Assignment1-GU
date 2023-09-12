7689d2cac28e7c45caec147961b4b5f2694df206
Emmaolmas <emma.olmas@gmail.com>
2023-09-12 19:29:38 +0200

    Update Task 4

--- Task4.java
+++ Task4.java
@@ -2,18 +2,25 @@ public class Task4 {
 
     public static void highestAndLowestScore(int[] studentScores) {
 
-        int highest = studentScores[0];
-        int secondHighest = studentScores[0];
-        int lowest = studentScores[0];
-        int secondLowest = studentScores[0];
+        int highest = 0;
+        int secondHighest = 0;
+        int lowest = 100;
+        int secondLowest = 100;
 
         for (int i = 0; i < studentScores.length; i++) {
+            int currentScore = studentScores[i];
 
-            if (highest < studentScores[i]) {
-                highest = studentScores[i];
+            if (currentScore > highest) {
+                secondHighest = highest;
+                highest = currentScore;
+            } else if (currentScore > secondHighest) {
+                secondHighest = currentScore;
             }
-            if (lowest > studentScores[i]) {
-                lowest = studentScores[i];
+            if (currentScore < lowest) {
+                secondLowest = lowest;
+                lowest = currentScore;
+            } else if (currentScore < secondLowest) {
+                secondLowest = currentScore;
             }
         }
         System.out.println("The two lowest scores provided are " + lowest + ", and " + secondLowest);
